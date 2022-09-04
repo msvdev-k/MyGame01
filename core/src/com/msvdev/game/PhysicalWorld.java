9961a09888f269cb14ed949649517121346895fd
msvdev-k <msvdev.k@gmail.com>
2022-09-04 13:50:14 +0300

    Homework 05

---
+++ core/src/com/msvdev/game/PhysicalWorld.java
@@ -0,0 +1,241 @@
+package com.msvdev.game;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+
+/**
+ * Класс, описывающий физический мир уровня
+ */
+public class PhysicalWorld {
+
+    /**
+     * Ускорение свободного падения
+     */
+    public static final float ACCELERATION_OF_GRAVITY = 9.81f;
+
+    /**
+     * Pixel per meter - константа, определяющая масштаб при переходе
+     * от размеров растрового изображения к размерам реального мира.
+     * PPM = количеству точек на один метр
+     */
+    public static final float PPM = 100f;
+
+
+    // Экземпляр класса, описывающего физический мир
+    private final World world;
+
+    // Отображение тел (для отладки)
+    private final Box2DDebugRenderer debugRenderer;
+
+
+    // Константы, определяющий дискретизацию физического мира
+    private static final float TIME_STEP = 1f/60;
+    private static final int VELOCITY_ITERATIONS = 3;
+    private static final int POSITION_ITERATIONS = 3;
+
+    // Переменная для отслеживания изменения времени в физическом мире
+    private float deltaTimeAccumulator = 0;
+
+
+
+    /**
+     * Основной конструктор класса
+     */
+    public PhysicalWorld() {
+        world = new World(new Vector2(0, -ACCELERATION_OF_GRAVITY), true);
+        debugRenderer = new Box2DDebugRenderer();
+    }
+
+
+
+    /**
+     * Добавить в физический мир твёрдые поверхности
+     * @param mapObjects коллекция твёрдых поверхностей
+     */
+    public void addSolidSurfaces(MapObjects mapObjects) {
+
+        for (MapObject mapObject: mapObjects) {
+
+            // Свойства тела
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+
+
+            // Свойства части тела
+            FixtureDef fixtureDef = new FixtureDef();
+
+            // Форма тела
+            PolygonShape polygonShape = new PolygonShape();
+
+
+            // Прямоугольный объект
+            if (mapObject instanceof RectangleMapObject) {
+                RectangleMapObject rectangleMapObject = (RectangleMapObject) mapObject;
+
+                Rectangle rectangle = rectangleMapObject.getRectangle();
+
+                bodyDef.position.set((rectangle.x + 0.5f * rectangle.width) / PPM, (rectangle.y + 0.5f * rectangle.height) / PPM);
+                polygonShape.setAsBox(0.5f * rectangle.width / PPM, 0.5f * rectangle.height / PPM);
+
+
+            // Полигональный объект
+            } else if (mapObject instanceof PolygonMapObject) {
+                PolygonMapObject polygonMapObject = (PolygonMapObject) mapObject;
+
+                Polygon polygon = polygonMapObject.getPolygon();
+
+                float[] vertices = polygon.getVertices();
+                for (int i = 0; i < vertices.length; i++) {
+                    vertices[i] /= PPM;
+                }
+
+                bodyDef.position.set(polygon.getX() / PPM, polygon.getY() / PPM);
+                polygonShape.set(vertices);
+
+            }
+
+
+            // Форма
+            fixtureDef.shape = polygonShape;
+
+            // Трение
+            Object friction = mapObject.getProperties().get("friction");
+            fixtureDef.friction = friction != null ? (float) friction : 0;
+
+            // Плотность
+            fixtureDef.density = 1f;
+
+            // Восстановление при ударе (эластичность)
+            fixtureDef.restitution = 0f;
+
+
+            // Создание твёрдого тела в физическом мире
+            Body body = world.createBody(bodyDef);
+            body.createFixture(fixtureDef);//.setUserData("стена");
+
+            // Освобождение ресурсов
+            polygonShape.dispose();
+        }
+    }
+
+
+
+
+    /**
+     * Добавить объект физического мира
+     * @param object добавляемый объект
+     * @return экземпляр класса твёрдого тела
+     */
+    public Body addObject(RectangleMapObject object) {
+
+        // Прямоугольник, описывающий объект
+        Rectangle rect = object.getRectangle();
+
+        // Тип объекта (тела): динамический, кинетический или статический (по умолчанию)
+        String bodyType = (String) object.getProperties().get("BodyType");
+
+
+        // Свойства тела
+        BodyDef def = new BodyDef();
+
+        // Тип тела
+        if (bodyType != null) {
+
+            if (bodyType.equalsIgnoreCase("StaticBody")) {
+                def.type = BodyDef.BodyType.StaticBody;
+
+            } else if (bodyType.equalsIgnoreCase("DynamicBody")) {
+                def.type = BodyDef.BodyType.DynamicBody;
+
+            } else if (bodyType.equalsIgnoreCase("KinematicBody")) {
+                def.type = BodyDef.BodyType.KinematicBody;
+
+            } else {
+                def.type = BodyDef.BodyType.StaticBody;
+            }
+
+        } else {
+            def.type = BodyDef.BodyType.StaticBody;
+        }
+
+        // Начальная позиция тела
+        def.position.set((rect.x + 0.5f * rect.width) / PPM, (rect.y + 0.5f * rect.height) / PPM);
+        def.gravityScale = 1f; //(float) object.getProperties().get("gravityScale");
+
+
+        // Свойства части тела
+        FixtureDef fdef = new FixtureDef();
+
+        // Геометрия составной части тела
+        PolygonShape polygonShape = new PolygonShape();
+        // Тело квадратное
+        polygonShape.setAsBox(0.5f * rect.width / PPM, 0.5f * rect.height / PPM);
+
+        // Другие доступные формы
+        CircleShape circleShape;
+        ChainShape chainShape;
+        EdgeShape edgeShape;
+
+        // Форма
+        fdef.shape = polygonShape;
+        // Трение
+        fdef.friction = 0.2f;
+        // Плотность
+        fdef.density = 1f;
+        // Восстановление при ударе (эластичность)
+        fdef.restitution = 0f; // (float) object.getProperties().get("restitution");
+
+
+        // Создание твёрдого тела в физическом мире
+        Body body = world.createBody(def);
+        body.createFixture(fdef);//.setUserData("стена");
+        body.setFixedRotation(true);
+
+        // Освобождение ресурсов
+        polygonShape.dispose();
+
+
+        return body;
+    }
+
+
+    /**
+     * Вычисление нового события происходящего в физическом мире
+     * @param deltaTime время в секундах, прошедшее от предыдущего события
+     */
+    public void doPhysicsStep(float deltaTime) {
+
+        deltaTimeAccumulator += deltaTime;
+
+        while (deltaTimeAccumulator >= TIME_STEP) {
+            world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+            deltaTimeAccumulator -= TIME_STEP;
+        }
+    }
+
+
+    /**
+     * Отрисовка контуров объектов физического мира (для отладки)
+     * @param camera камера
+     */
+    public void debugDraw(OrthographicCamera camera) {
+        debugRenderer.render(world, camera.combined);
+    }
+
+
+    /**
+     * Освобождение ресурсов
+     */
+    public void dispose() {
+        world.dispose();
+        debugRenderer.dispose();
+    }
+}
--- core/src/com/msvdev/game/screens/GameScreen.java
+++ core/src/com/msvdev/game/screens/GameScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -16,7 +15,10 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.msvdev.game.PhysicalWorld;
 import com.msvdev.game.busterbanny.BusterBunnyWalk;
 
 
@@ -38,8 +40,16 @@ public class GameScreen implements Screen {
 
     private final OrthographicCamera camera;
     private final OrthogonalTiledMapRenderer mapRenderer;
-    private final Rectangle mapSize;
+//    private final Rectangle mapSize;
     private final ShapeRenderer shapeRenderer;
+
+
+    // Физический мир
+    private PhysicalWorld physicalWorld;
+    private final int[] bg;
+    private final int[] l1;
+    private final Body heroBody;
+    private final Rectangle heroRect;
 
 
     public GameScreen(Game game) {
@@ -62,18 +72,42 @@ public class GameScreen implements Screen {
         shapeRenderer = new ShapeRenderer();
 
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camera.zoom = 1f;
+        camera.zoom = 0.5f;
 
         // Загрузка карты
         TiledMap map = new TmxMapLoader().load("map/Level_01.tmx");
         mapRenderer = new OrthogonalTiledMapRenderer(map);
 
-        RectangleMapObject tmp = (RectangleMapObject) map.getLayers().get("Объекты").getObjects().get("Камера"); //выбор объекта по имени
-        camera.position.x = tmp.getRectangle().x;
-        camera.position.y = tmp.getRectangle().y;
+        // Фоновый слой
+        bg = new int[1];
+        bg[0] = map.getLayers().getIndex("Задний фон");
 
-        tmp = (RectangleMapObject) (map.getLayers().get("Объекты").getObjects().get("Граница карты"));
-        mapSize = tmp.getRectangle();
+        // Игровые слои
+        l1 = new int[2];
+//        l1[0] = map.getLayers().getIndex("Слой 2");
+//        l1[1] = map.getLayers().getIndex("Слой 3");
+
+        physicalWorld = new PhysicalWorld();
+
+
+        RectangleMapObject tmp = (RectangleMapObject) map.getLayers().get("Персонажи").getObjects().get("Герой"); //выбор объекта по имени
+//        camera.position.x = tmp.getRectangle().x;
+//        camera.position.y = tmp.getRectangle().y;
+//        RectangleMapObject tmp = (RectangleMapObject) map.getLayers().get("сеттинг").getObjects().get("hero"); //выбор объекта по имени
+        heroRect = tmp.getRectangle();
+        heroBody = physicalWorld.addObject(tmp);
+
+
+        physicalWorld.addSolidSurfaces(map.getLayers().get("Твёрдые горизонтальные поверхности").getObjects());
+        physicalWorld.addSolidSurfaces(map.getLayers().get("Твёрдые вертикальные поверхности").getObjects());
+//        Array<RectangleMapObject> objects = map.getLayers().get("Твёрдые горизонтальные поверхности").getObjects().getByType(RectangleMapObject.class);
+//
+//        for (int i = 0; i < objects.size; i++) {
+//            physicalWorld.addObject(objects.get(i));
+//        }
+//
+//        tmp = (RectangleMapObject) (map.getLayers().get("Объекты").getObjects().get("Граница карты"));
+//        mapSize = tmp.getRectangle();
 
 
     }
@@ -92,88 +126,91 @@ public class GameScreen implements Screen {
 
 
     @Override
-    public void render(float delta) {
-
-        // Установка камеры
-        float STEP = 3;
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && mapSize.x < (camera.position.x - STEP))
-            camera.position.x -= STEP;
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && (mapSize.x + mapSize.width) > (camera.position.x + STEP))
-            camera.position.x += STEP;
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) camera.position.y += STEP;
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) camera.position.y -= STEP;
-
-        if (Gdx.input.isKeyPressed(Input.Keys.P)) camera.zoom += 0.01f;
-        if (Gdx.input.isKeyPressed(Input.Keys.O) && camera.zoom > 0) camera.zoom -= 0.01f;
-
-        camera.update();
-
-        // Очистка экрана
-        ScreenUtils.clear(0.5f, 1f, 0.5f, 1f);
-
+    public void render(float deltaTime) {
 
         // Текущие координаты мыши
-        float x = Gdx.input.getX();
-        float y = Gdx.graphics.getHeight() - Gdx.input.getY();
-
-
-        // Отображение карты
-        mapRenderer.setView(camera);
-        mapRenderer.render();
-
-
-        // Прямоугольник, охватывающий карту
-        shapeRenderer.setProjectionMatrix(camera.combined);
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.setColor(Color.GOLD);
-        shapeRenderer.rect(mapSize.x, mapSize.y, mapSize.width, mapSize.height);
-        shapeRenderer.end();
+        float mouseX = Gdx.input.getX();
+        float mouseY = Gdx.graphics.getHeight() - Gdx.input.getY();
 
 
         // Отработка нажатия на кнопку Выход
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) &&
-                rectangleExitButton.contains(x, y)) {
+                rectangleExitButton.contains(mouseX, mouseY)) {
 
             dispose();
             game.setScreen(new StartMenuScreen(this.game));
         }
 
 
+
+        // Управление камерой
+        if (Gdx.input.isKeyPressed(Input.Keys.P)) {
+            camera.zoom += 0.01f;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.O) && camera.zoom > 0) {
+            camera.zoom -= 0.01f;
+        }
+
+
+
+        // Движение персонажа вправо или влево
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            heroBody.applyForceToCenter(new Vector2(-0.2f, 0), true);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            heroBody.applyForceToCenter(new Vector2(0.2f, 0), true);
+        }
+
         // Поворот персонажа вправо или влево
         if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
-            forwardMovement = true;
             busterBunnyWalk.setWalkForward(true);
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
-            forwardMovement = false;
             busterBunnyWalk.setWalkForward(false);
         }
 
 
-        // Текущий кадр
-        busterBunnyWalk.incrementTime(delta);
-        TextureRegion frame = busterBunnyWalk.getFrame();
+
+        // Позиция камеры
+        camera.position.x = heroBody.getPosition().x * PhysicalWorld.PPM;
+        camera.position.y = heroBody.getPosition().y * PhysicalWorld.PPM;
+        camera.update();
+
+        // Очистка экрана
+        ScreenUtils.clear(1f, 1f, 1f, 1f);
 
 
-        // Перемещение координаты
-//        if (forwardMovement) {
-//            xRun += vxRun * delta;
-//            if (xRun >= Gdx.graphics.getWidth() - frame.getRegionWidth()) {
-//                forwardMovement = false;
-//                busterBunnyWalk.setWalkForward(false);
-//            }
-//        } else {
-//            xRun -= vxRun * delta;
-//            if (xRun <= 0) {
-//                forwardMovement = true;
-//                busterBunnyWalk.setWalkForward(true);
-//            }
-//        }
+
+        // Отображение заднего фона карты
+        mapRenderer.setView(camera);
+        mapRenderer.render(bg);
 
 
-        // Отрисовка фрейма
+        // Текущий кадр персонажа
+        busterBunnyWalk.incrementTime(deltaTime);
+        TextureRegion heroFrame = busterBunnyWalk.getFrame();
+
+
+        // Отображение героя
+//        batch.setProjectionMatrix(camera.combined);
+//        heroRect.x = heroBody.getPosition().x * PhysicalWorld.PPM - heroRect.width/2;
+//        heroRect.y = heroBody.getPosition().y * PhysicalWorld.PPM - heroRect.height/2;
+        float heroX = 0.5f * Gdx.graphics.getWidth() - 0.5f * (heroRect.width + 3) / camera.zoom;
+        float heroY = 0.5f * Gdx.graphics.getHeight() - 0.5f * (heroRect.height + 3) / camera.zoom;
         batch.begin();
-        batch.draw(frame, 0.5f * Gdx.graphics.getWidth(), 0.5f * Gdx.graphics.getHeight());
+        batch.draw(heroFrame, heroX, heroY, heroFrame.getRegionWidth() / camera.zoom, heroFrame.getRegionHeight() / camera.zoom);
+        batch.end();
+
+
+
+        // Отображение переднего фона карты
+        //mapRenderer.render(l1);
+
+
+
+        // Отображение кнопок меню
+        batch.begin();
+        //batch.draw(heroFrame, 0.5f * Gdx.graphics.getWidth(), 0.5f * Gdx.graphics.getHeight());
         batch.draw(exitButton,
                 rectangleExitButton.getX(),
                 rectangleExitButton.getY(),
@@ -181,7 +218,17 @@ public class GameScreen implements Screen {
                 rectangleExitButton.getHeight()
         );
         batch.end();
+
+
+
+        // Перерасчёт положения тел
+        physicalWorld.doPhysicsStep(deltaTime);
+//        physicalWorld.debugDraw(camera);
+
     }
+
+
+
 
     @Override
     public void resize(int width, int height) {
@@ -196,6 +243,9 @@ public class GameScreen implements Screen {
         batch.dispose();
         batch = new SpriteBatch();
     }
+
+
+
 
     @Override
     public void pause() {
@@ -212,6 +262,8 @@ public class GameScreen implements Screen {
 
     }
 
+
+
     @Override
     public void dispose() {
         batch.dispose();
@@ -219,5 +271,6 @@ public class GameScreen implements Screen {
         exitButton.dispose();
         mapRenderer.dispose();
         shapeRenderer.dispose();
+        physicalWorld.dispose();
     }
 }
